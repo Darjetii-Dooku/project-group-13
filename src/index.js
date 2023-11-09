7bff8b7cf8a7df777fce1ff1a838ad834352e774
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-11-09 18:29:26 +0100

    as

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./js/images-api";
+
+import "./js/images-api";
+
+import "./js/header";
+
+import "./js/all-categories";
+
+import "./js/support";
+
+import "./js/best-sellers";
+
+import "./js/filtered";
+
+import "./js/modal-window";
+
+//test
