64c48eac4e8ec0a59eb81e6edde11dbe24c518a6
Darjetii-Dooku <7220339@gmail.com>
2023-11-09 17:26:39 +0200

    base files set up

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./js/images-api";
+
+import "./js/images-api";
+
+import "./js/header";
+
+import "./js/all-categories";
+
+import "./js/support";
+
+import "./js/best-sellers";
+
+import "./js/filtered";
+
+import "./js/modal-window";
---
+++ src/js/index-shopping.js
@@ -0,0 +1,4 @@
+import "./images-api.js"
+import "./header.js"
+import "./support.js"
+import "./shopping.js"
