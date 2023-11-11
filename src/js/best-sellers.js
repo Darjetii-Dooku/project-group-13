135214f44da6b6e416a950475fe99e88ae50f7c8
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-11-11 23:20:30 +0100

    Update best-sellers.js

---
+++ src/js/best-sellers.js
@@ -0,0 +1,36 @@
+import { getTopBooks } from "./images-api"
+
+getTopBooks()
+.then(data => {
+    const categoryListContainer = document.querySelector('.book-category-list');
+
+    data.forEach(category => {
+  
+      const categoryTitle = document.createElement('h3');
+      categoryTitle.textContent = `Category - ${category.list_name}`.toUpperCase();
+
+      const booksList = document.createElement('ul');
+
+      category.books.forEach(book => {
+        const bookItem = document.createElement('li');
+        bookItem.innerHTML = `
+          <img src="${book.book_image}" alt="${book.title}" width="180px" height="256px" />
+          <p>${book.title}</p>
+          <p>${book.author}</p>
+        `;
+        //
+        
+        booksList.appendChild(bookItem);
+      });
+
+      categoryListContainer.appendChild(categoryTitle);
+      categoryListContainer.appendChild(booksList);
+
+      const seeMoreButton = document.createElement('button');
+      seeMoreButton.type = 'button';
+      seeMoreButton.textContent = 'See More';
+      categoryListContainer.appendChild(seeMoreButton);
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
