16958ab1fe8c1a2afc3be7d6dc6368ab003f03f9
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-11-11 13:17:55 +0100

    Deleting fetch update

--- src/js/best-sellers.js
+++ src/js/best-sellers.js
@@ -1,7 +1,7 @@
+import { getTopBooks } from "/src/js/images-api"
 
-fetch(`https://books-backend.p.goit.global/books/top-books`)
-  .then(res => res.json())
-  .then(data => {
+getTopBooks()
+.then(data => {
     const categoryListContainer = document.querySelector('.book-category-list');
 
     data.forEach(category => {
@@ -13,18 +13,8 @@ fetch(`https://books-backend.p.goit.global/books/top-books`)
 
       category.books.forEach(book => {
         const bookItem = document.createElement('li');
-        //Якщо треба додати клас на лі, використовуй наступне
-        //bookItem.classList.add('your-class-name');  Замість 'your-class-name' 
-        //Для назви секції(categoryItem) - юзай код зверху, якщо треба клас
-        //Якщо треба картку запхати в список - просто як звичайно огорни в список і буде норм
-        //Я чекнув, воно все працює, як треба
-        //Класи в шаблон картки додавай в розмітку знизу, воно теж працює
-        //Якщо якась шляпа з mobile first - я прогнав код через GPT, вже є інфа, як то зробити
-        // Напишеш в телеграмі і я скину скрін
-        
-  
         bookItem.innerHTML = `
-          <img src="${book.book_image}" alt="${book.title} width="180px" height="256px"" />
+          <img src="${book.book_image}" alt="${book.title}" width="180px" height="256px" />
           <p>${book.title}</p>
           <p>${book.author}</p>
         `;
