cf8a646c0f16643b872c3edd651f1ca3e829a56d
Valerjan-1 <134737688+Valerjan-1@users.noreply.github.com>
2023-11-12 00:23:13 +0200

    add listener

--- src/js/all-categories.js
+++ src/js/all-categories.js
@@ -1,6 +1,7 @@
 import { getCategoryList } from './images-api';
+import { getCategory } from './images-api';
 const allList = document.querySelector(`.all-categories`);
-
+const listCategory = document.querySelector(`.book-category-list`);
 getCategoryList()
   .then(data => {
     data
@@ -10,9 +11,38 @@ getCategoryList()
 
         return allList.insertAdjacentHTML(
           `beforeend`,
-          `<li class="list-category"><a href="">${list_name}</a></li>`
+          `<li ><a class="list-category" href="">${list_name}</a></li>`
         );
       })
       .join(``);
   })
   .catch(error => console.log(error));
+
+allList.addEventListener(`click`, nameCategories);
+
+function nameCategories(e) {
+  e.preventDefault();
+  const categoriesName = e.target.textContent;
+  if (categoriesName) {
+    getCategory(categoriesName)
+      .then(data => {
+        data
+          .map((list, i) => {
+            console.dir(i);
+            return (listCategory.innerHTML = createMarkup(list));
+          })
+          .join(``);
+      })
+      .catch(err => console.log(err));
+  } else {
+  }
+}
+
+function createMarkup(list) {
+  const { book_image, title, author } = list;
+  return `
+          <img src="${book_image}" alt="${title}" width="180px" height="256px" />
+          <p>${title}</p>
+          <p>${author}</p>
+        `;
+}
