4e4dcda33835b5300d10a647d95e5f8ed4600173
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-11-12 20:33:16 +0200

    header4

--- src/js/header.js
+++ src/js/header.js
@@ -10,31 +10,13 @@ elements.theme.addEventListener('change', function () {
 
   const useElement = elements.bookshelfIcon.querySelector('use');
   if (elements.theme.checked) {
-    // Темна тема
     useElement.setAttribute('href', '/css/sprite.svg#icon-Bookshelf_white');
     useElement.setAttribute('href', '/css/sprite.svg#icon-align-left_white');
   } else {
-    // Світла тема
     useElement.setAttribute('href', '/css/sprite.svg#icon-Bookshelf');
     useElement.setAttribute('href', '/css/sprite.svg#icon-align-left');
   }
 });
 
-// Опціональний код для перевірки попереднього стану теми при завантаженні сторінки
-// const isDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-// if (isDarkTheme) {
-//   elements.bookshelfIcon.querySelector('use').setAttribute('href', '/css/sprite.svg#icon-Bookshelf_white');
-//   elements.bookshelfIcon.querySelector('use').style.display = 'none';
-<<<<<<< Updated upstream
-// }
 
 
-
-
-
-
-
-
-=======
-// }
->>>>>>> Stashed changes
