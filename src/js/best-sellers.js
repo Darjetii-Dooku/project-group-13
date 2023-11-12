a37aeae4f21958d5e336e2c9c02527a7f8fa51d8
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-11-12 13:00:14 +0200

    update best sellers

--- src/js/best-sellers.js
+++ src/js/best-sellers.js
@@ -7,18 +7,23 @@ getTopBooks()
     data.forEach(category => {
   
       const categoryTitle = document.createElement('h3');
-      categoryTitle.textContent = `Category - ${category.list_name}`.toUpperCase();
+      categoryTitle.textContent = `${category.list_name}`.toUpperCase();
 
       const booksList = document.createElement('ul');
+      // add class
+      booksList.className = 'bs-book-list';
 
       category.books.forEach(book => {
         const bookItem = document.createElement('li');
+        // add class
+        bookItem.className = 'bs-book-item';
+        
+      // update code with add class
         bookItem.innerHTML = `
-          <img src="${book.book_image}" alt="${book.title}" width="180px" height="256px" />
-          <p>${book.title}</p>
-          <p>${book.author}</p>
+          <img src="${book.book_image}" alt="${book.title}"" />
+          <p class="title-book">${book.title}</p>
+          <p class="author-book">${book.author}</p>
         `;
-        //
         
         booksList.appendChild(bookItem);
       });
@@ -28,7 +33,16 @@ getTopBooks()
 
       const seeMoreButton = document.createElement('button');
       seeMoreButton.type = 'button';
-      seeMoreButton.textContent = 'See More';
+      // add class
+      seeMoreButton.classList.add('bs-buttom');
+
+      // update code with add class
+      // seeMoreButton.textContent = 'See More';
+      const seeMoreText = document.createElement('span');
+      seeMoreText.textContent = 'See More';
+      seeMoreText.classList.add('bs-buttom-name');
+      seeMoreButton.appendChild(seeMoreText);
+
       categoryListContainer.appendChild(seeMoreButton);
     });
   })
